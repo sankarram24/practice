e1b7b04cd944cc5368d25bcdbb82c315c1101947
sankarram24 <sankarram242@gmail.com>
2016-09-28 11:33:14 +0530

    Create arrraylist.java

---
+++ arrraylist.java
@@ -0,0 +1,27 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class arrraylist
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		ArrayList arraylist=new ArrayList();
+		
+		arraylist.add("1");
+		arraylist.add("2");
+		arraylist.add("3");
+		arraylist.add("4");
+		arraylist.add(3,"4");
+		
+		
+		for(int i=0;i<arraylist.size();i++)
+		{
+			System.out.println(arraylist.get(i));
+		}
+	}
+}
